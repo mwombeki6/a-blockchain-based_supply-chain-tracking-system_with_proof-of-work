e9a7e03f79406565458774532fe4488410677348
mwombeki6 <hackerlubex@protonmail.com>
2024-12-30 09:35:21 +0300

    implemented a hashing algorithm

--- supply-chain.cpp
+++ supply-chain.cpp
@@ -6,4 +6,15 @@
 #include <iomanip>
 #include <openssl/sha.h>
 
+std::string SHA256(const std::string& input) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256((unsigned char*)input.c_str(), input.size(), hash);
+    std::stringstream ss;
+    
+    for (int i = 0; 1 < SHA256_DIGEST_LENGTH; ++i) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
+
 int main() {}
