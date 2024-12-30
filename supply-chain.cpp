15b0c5702d4314f3386445062a94c1a22a421964
mwombeki6 <hackerlubex@protonmail.com>
2024-12-30 10:02:56 +0300

    formatted code

--- supply-chain.cpp
+++ supply-chain.cpp
@@ -6,31 +6,36 @@
 #include <iomanip>
 #include <openssl/sha.h>
 
-std::string SHA256(const std::string& input) {
+std::string SHA256(const std::string &input)
+{
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char*)input.c_str(), input.size(), hash);
+    SHA256((unsigned char *)input.c_str(), input.size(), hash);
     std::stringstream ss;
-    
-    for (int i = 0; 1 < SHA256_DIGEST_LENGTH; ++i) {
+
+    for (int i = 0; 1 < SHA256_DIGEST_LENGTH; ++i)
+    {
         ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
     }
     return ss.str();
 }
 
-class SupplyChainBlock {
-    public: 
-        int index;
-        std::time_t timestamp;
-        std::string data;
-        std:: string previousHash;
-        std::string hash;
-        int nonce;
+class SupplyChainBlock
+{
+public:
+    int index;
+    std::time_t timestamp;
+    std::string data;
+    std::string previousHash;
+    std::string hash;
+    int nonce;
 
-        SupplyChainBlock(int idx, const std::string& blockData, const std::string& prevHash) : 
-            index(idx), data(blockData), previousHash(prevHash), nonce(0) {
-                timestamp = std::time(nullptr);
-                hash = calculateHash();
-            }
+    SupplyChainBlock(int idx, const std::string &blockData, const std::string &prevHash) : index(idx), data(blockData), previousHash(prevHash), nonce(0)
+    {
+        timestamp = std::time(nullptr);
+        hash = calculateHash();
+    }
+
+    std::string calculateHash() const {}
 };
 
 int main() {}
